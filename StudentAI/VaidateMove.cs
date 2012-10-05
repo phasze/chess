f580d851062592dfcc7dd442ec2475fa22f87443
Rick C <phase12+git@gmail.com>
2012-10-05 13:02:50 -0600

    bah

--- StudentAI/VaidateMove.cs
+++ StudentAI/VaidateMove.cs
@@ -21,25 +21,71 @@ namespace StudentAI
         /// </summary>
         /// <param name="board">board state before move</param>
         /// <param name="move">move the piece wants to make</param>
-        /// <returns></returns>
+        /// <returns>true if valid move, false is invalid</returns>
         public static bool WhiteKing(ChessBoard board, ChessMove move)
         {
             //TODO NOT DONE, still need to finish this
-            //validate the move is still on the board
-            if (!StillOnBoard(move.To.X, move.To.Y))
+            //validate the move is still on the board as well as
+            //  ensure new move is onto an enemy piece or empty space
+            if (!OnBoardAndKillOrEmpty(move.To.X, move.To.Y,board,ChessColor.White))
                 return false;
 
             //validate it doesn't put self into check
 
 
+            //if all checks pass then
+            return true;
         }
 
-        //checks if the move is still located on the board
-        private static bool StillOnBoard(int X, int Y)
+        private static bool checkEmpty(int X, int Y, ChessBoard board)
         {
-            if (X < 8 && X > -1 && Y < 8 && Y > -1)
+            if (board[X, Y] == ChessPiece.Empty)
                 return true;
             return false;
+        }
+
+
+        //checks if the move is still located on the board & is moved onto empty or kill spot
+        private static bool OnBoardAndKillOrEmpty(int X, int Y,ChessBoard board,ChessColor color)
+        {
+            if (X > 8 || X < 0 || Y > 8 || Y < 0)
+                return false;
+
+            if(board[X,Y] == ChessPiece.Empty)
+            {
+                if(color==ChessColor.White)
+                {
+                    switch (board[X,Y])
+                    {
+                        case ChessPiece.BlackPawn:
+                        case ChessPiece.BlackKnight:
+                        case ChessPiece.BlackBishop:
+                        case ChessPiece.BlackQueen:
+                        case ChessPiece.BlackRook:
+                        case ChessPiece.BlackKing:
+                            return true;
+                        default:
+                            return false;
+                    }
+                }
+                else if(color==ChessColor.Black)
+                {
+                    switch (board[X, Y])
+                    {
+                        case ChessPiece.WhiteBishop:
+                        case ChessPiece.WhiteKing:
+                        case ChessPiece.WhiteKnight:
+                        case ChessPiece.WhitePawn:
+                        case ChessPiece.WhiteQueen:
+                        case ChessPiece.WhiteRook:
+                            return true;
+                        default:
+                            return false;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
