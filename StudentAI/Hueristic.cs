8bd0e51bdd36866915d75ef6e321e85813589fd7
Rick C <phase12+git@gmail.com>
2012-10-09 19:26:15 -0600

    contents of hueristic

--- StudentAI/Hueristic.cs
+++ StudentAI/Hueristic.cs
@@ -7,5 +7,13 @@ namespace StudentAI
 {
     class Hueristic
     {
+        public UvsChess.ChessBoard BoardAfterMove = null;
+        public UvsChess.ChessMove TheMove=null;
+        public Hueristic(UvsChess.ChessBoard board, UvsChess.ChessMove move)
+        {
+            BoardAfterMove = board;
+            BoardAfterMove.MakeMove(move);
+            TheMove = move;
+        }
     }
 }
