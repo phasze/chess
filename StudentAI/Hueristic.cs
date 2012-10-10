0f0837c8aef069934885f60edaf4af648177a06e
Rick C <phase12+git@gmail.com>
2012-10-09 19:33:05 -0600

    Simple Hueristic put in

--- StudentAI/Hueristic.cs
+++ StudentAI/Hueristic.cs
@@ -2,18 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using  UvsChess;
 
 namespace StudentAI
 {
     class Hueristic
     {
-        public UvsChess.ChessBoard BoardAfterMove = null;
-        public UvsChess.ChessMove TheMove=null;
-        public Hueristic(UvsChess.ChessBoard board, UvsChess.ChessMove move)
+        public ChessBoard BoardAfterMove = null;
+        public ChessMove TheMove=null;
+        public Hueristic(ChessBoard board, ChessMove move)
         {
             BoardAfterMove = board;
             BoardAfterMove.MakeMove(move);
             TheMove = move;
+        }
+
+        /// <summary>
+        /// The lower the number returned the better off you are
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="colorOfEnemyTeam"></param>
+        /// <returns></returns>
+        public static int CalculateHueristicBasic(ChessBoard board, ChessColor colorOfEnemyTeam)
+        {
+            int score = 0;
+            for(int x=0;x<8;x++)
+            {
+                for(int y=0;y<8;y++)
+                {
+                    if (colorOfEnemyTeam == ChessColor.Black && board[x, y] < ChessPiece.Empty) //if black
+                        score++;
+                    else if (colorOfEnemyTeam == ChessColor.White && board[x, y] > ChessPiece.Empty) //if black
+                        score++;
+                }
+            }
+
+            return score;
+
         }
     }
 }
