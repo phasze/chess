1bbbe0f3277adfdc2f50ab71d0885d2347f5c11d
Rick C <phase12+git@gmail.com>
2012-10-17 21:45:09 -0600

    asdf

--- StudentAI/Hueristic.cs
+++ StudentAI/Hueristic.cs
@@ -61,6 +61,37 @@ namespace StudentAI
 
         }
 
+
+        /// <summary>
+        /// The lower the number returned the better off you are
+        /// </summary>
+        /// <param name="board"></param>
+        /// <param name="colorOfEnemyTeam"></param>
+        /// <returns></returns>
+        int CalculateHueristicAdvanced(ChessBoard board, ChessColor colorOfEnemyTeam)
+        {
+            int scoreB = 0;
+            int scoreW = 0;
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    if (colorOfEnemyTeam == ChessColor.Black && BoardAfterMove[x, y] < ChessPiece.Empty) //if black
+                        scoreB += StudentAI.Piece.CalculatePieceValue(BoardAfterMove[x, y]);
+                    else if (colorOfEnemyTeam == ChessColor.White && BoardAfterMove[x, y] > ChessPiece.Empty) //if white
+                        scoreW += StudentAI.Piece.CalculatePieceValue(BoardAfterMove[x, y]);
+                }
+            }
+
+            return colorOfEnemyTeam == ChessColor.Black
+                       ? scoreB - scoreW +
+                         StudentAI.Piece.CalculatePieceActionValue(board[TheMove.From.X, TheMove.From.Y])
+                       : scoreW - scoreB +
+                         StudentAI.Piece.CalculatePieceActionValue(board[TheMove.From.X, TheMove.From.Y]);
+            return score - StudentAI.Piece.CalculatePieceActionValue(board[TheMove.From.X, TheMove.From.Y]);
+
+        }
+
         /// <summary>
         /// High Number = better board for your color
         /// </summary>
