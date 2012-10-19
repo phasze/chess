8ff9148d2733cc3e5cd0ef0f773880a720572b6d
srichins <shaun@sdrichins.com>
2012-10-19 13:41:43 -0600

    updating to latest value

--- StudentAI/Minimax.cs
+++ StudentAI/Minimax.cs
@@ -48,16 +48,19 @@ namespace StudentAI
             foreach (var hmove in HueristicMoves)
             {
                 var tempBoard = board.Clone();
-                tempBoard.MakeMove(hmove.TheMove);
+                if (hmove.TheMove!= null && hmove.TheMove.From != null)
+                {
+                    tempBoard.MakeMove(hmove.TheMove);
 
-                ChessMove oppositemove = getMinimax(AI, tempBoard, oppositeColor, depth - 1); //get best move of the other color
+                    ChessMove oppositemove = getMinimax(AI, tempBoard, oppositeColor, depth - 1); //get best move of the other color
 
-                //TODO this will call a null move if checkmate or no moves, needs fixing
-                if (oppositemove.To == null && oppositemove.From == null)
-                    tempBoard.MakeMove(oppositemove); //update the board with the new move
-                var oppositemovehueristic = new Hueristic(tempBoard, color); // calculate the score of the board
-                hmove.HValue -= oppositemovehueristic.HValue; // update our moves score based on return of projected other move
-                updatedhueristic.Add(hmove); // add new scored hueristic to new list
+                    //TODO this will call a null move if checkmate or no moves, needs fixing
+                    if (oppositemove.To == null && oppositemove.From == null)
+                        tempBoard.MakeMove(oppositemove); //update the board with the new move
+                    var oppositemovehueristic = new Hueristic(tempBoard, color); // calculate the score of the board
+                    hmove.HValue -= oppositemovehueristic.HValue; // update our moves score based on return of projected other move
+                    updatedhueristic.Add(hmove); // add new scored hueristic to new list
+                }
             }
             updatedhueristic.Sort((x, y) => x.HValue.CompareTo(y.HValue)); // sort the new list
             if (updatedhueristic.Count == 0)
